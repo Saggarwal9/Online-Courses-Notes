449aa32d19be018cc43db37c6cf775a077890573
Shubham Aggarwal <35288936+Saggarwal9@users.noreply.github.com>
2018-05-27 02:50:49 -0500

    Update basic.js

--- JS Notes/basic.js
+++ JS Notes/basic.js
@@ -1,5 +1,3 @@
-//File with some of my functions to practise javascript.
-
 function factorial(num){
 	if(num==0)
 		return 1;
@@ -13,8 +11,40 @@ function kebabToSnake(string){
 	console.log(string.replace(/-/g,"_"));
 }
 
+//Takes an array as an argument and returns true
+//if all elements in the array are identical
+function isUniform(array){
+	var val=array[0];
+	for(var i=0;i<array.length;i++){
+		if(val==array[i])
+			continue;
+		else
+			return false;
+	}
+	return true;
+}
+
+function sumArray(array){
+	var sum=0;
+	for(var i=0;i<array.length;i++)
+		sum+=array[i];
+	return sum;
+}
+
+//Takes an array as an arguments and pritns out the
+// elements in the array in reverse order
+function printReverse(array){
+	for(var i=array.length-1;i>=0;i--){
+		console.log(array[i]);
+	}
+}
+
+// printReverse([1,2,3,4,5,6]);
+console.log(sumArray([1,2,3,4,5,6]));
+console.log(sumArray([1,1,1,1,1,1]));
+
 // var number= prompt("Enter a number");
 // var fact=factorial(number);
 // console.log(fact);
-var string= "My-Name-is-shubham_aAgarwal";
-kebabToSnake(string);
+// var string= "My-Name-is-shubham_aAgarwal";
+// kebabToSnake(string);
